465e228514bbd116b0a5c00b812e643462dfbf62
MAEL <mael.petit@wanadoo.fr>
2017-11-20 16:23:17 +0100

    view.fxml

--- src/async/Run.java
+++ src/async/Run.java
@@ -20,14 +20,22 @@ public class Run {
         Generator generator = new GeneratorImpl(seqDistribution);
         ScheduledExecutorService service = new ScheduledThreadPoolExecutor(15);
         Channel channel1 = new Channel(generator, service, 100);
-        Channel channel2 = new Channel(generator, service, 1000);
+        Channel channel2 = new Channel(generator, service, 200);
+        Channel channel3 = new Channel(generator, service, 300);
+        Channel channel4 = new Channel(generator, service, 400);
         Display display1 = new Display("1");
         Display display2 = new Display("2");
+        Display display3 = new Display("3");
+        Display display4 = new Display("4");
         channel1.attach(display1);
         channel2.attach(display2);
+        channel3.attach(display3);
+        channel4.attach(display4);
         ((GeneratorImpl) generator).addAsyncObserver(channel1);
         ((GeneratorImpl) generator).addAsyncObserver(channel2);
+        ((GeneratorImpl) generator).addAsyncObserver(channel3);
+        ((GeneratorImpl) generator).addAsyncObserver(channel4);
 
-        service.scheduleAtFixedRate(()-> generator.generate(), 0, 100, TimeUnit.MILLISECONDS);
+        service.scheduleAtFixedRate(()-> generator.generate(), 0, 1000, TimeUnit.MILLISECONDS);
     }
 }
